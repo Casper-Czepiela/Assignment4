51e37e6a3cfa2a9ff945b67670534ff9cb64b27b
Casper-Czepiela <78100618+Casper-Czepiela@users.noreply.github.com>
2021-12-07 22:04:04 -0500

    Add files via upload

---
+++ github/assets/data/czepielcData.ts
@@ -0,0 +1,9 @@
+import { czepielc } from "src/app/czepielc";
+
+export const casperInfo: czepielc = {
+  sNum: '991627402',
+  sName: 'Casper Czepiela',
+  lName: 'czepielc',
+  campus: 'trafalgar',
+  aTitle: 'Assignment4'
+};
